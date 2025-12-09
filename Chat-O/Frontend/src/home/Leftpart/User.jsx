f1e83ae888cf11ec51c9b2173a369bdabddebe91
Amit37-r <2022ugcm025@nitjsr.ac.in>
2025-12-09 17:22:51 +0530

    chat-O

---
+++ Chat-O/Frontend/src/home/Leftpart/User.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import useConversation from "../../statemanage/useConversation.js";
+import { useSocketContext } from "../../context/SocketContext.jsx";
+
+function User({ user }) {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  const isSelected = selectedConversation?._id === user._id;
+  const { socket, onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(user._id);
+  return (
+    <div
+      className={`hover:bg-slate-600 duration-300 ${
+        isSelected ? "bg-slate-700" : ""
+      }`}
+      onClick={() => setSelectedConversation(user)}
+    >
+      <div className="flex space-x-4 px-8 py-3 hover:bg-slate-700 duration-300 cursor-pointer">
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
+          <div className="w-12 rounded-full">
+             <img src=" https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg "  />
+        
+          </div>
+        </div>
+        <div>
+          <h1 className=" font-bold">{user.name}</h1>
+          <span>{user.email}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default User;
---
+++ Chat-O/Frontend/src/home/Rightpart/Chatuser.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import useConversation from "../../statemanage/useConversation.js";
+import { useSocketContext } from "../../context/SocketContext.jsx";
+import { CiMenuFries } from "react-icons/ci";
+
+function Chatuser() {
+  const { selectedConversation } = useConversation();
+  const { onlineUsers } = useSocketContext();
+  const getOnlineUsersStatus = (userId) => {
+    return onlineUsers.includes(userId) ? "Online" : "Offline";
+  };
+
+  return (
+    <div className=" pl-5 pt-5 h-[12vh] flex space-x-4 bg-gray-700 hover:bg-gray-600 duration-300">
+      <div>
+        <div className="avatar online">
+          <div className="w-14 rounded-full">
+            <img src="https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1 className="text-xl">{selectedConversation.name}</h1>
+        <span className="text-sm">
+          {getOnlineUsersStatus(selectedConversation._id)}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Chatuser;
